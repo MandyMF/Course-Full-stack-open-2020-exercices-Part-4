22894f08f577c35fc2d9cecd63e621a534f1e994
MandyMF <mandy@fonoma.com>
2020-09-25 02:49:33 -0400

    Exercise 4.5*

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -9,10 +9,18 @@ const totalLikes = ( blogs ) => {
   }
 
   return blogs.reduce(reducer, 0)
+}
 
+const favoriteBlog=(blogs) => {
+  const reducer = (acc, cur) => {
+    return cur.likes > acc.likes ? cur : acc
+  }
+
+  return blogs.length > 0 ? blogs.reduce(reducer, blogs[0]) : null
 }
 
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
