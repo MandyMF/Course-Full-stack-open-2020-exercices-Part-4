1ab71f0f44c8b2edee01106e099ab03e26bf9a7a
MandyMF <mandy@fonoma.com>
2020-09-24 22:03:13 -0400

    Exercise 4.1

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const http = require('http')
+//const http = require('http')
 const express = require('express')
 const app = express()
 const cors = require('cors')
@@ -12,11 +12,14 @@ const blogSchema = new mongoose.Schema({
   likes: Number
 })
 
-/*blogSchema.set('toJSON',{
-  transform: () => {
-
+blogSchema.set('toJSON',{
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
   }
-})*/
+})
+
 
 const Blog = mongoose.model('Blog', blogSchema)
 
