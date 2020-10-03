caae70bda35ccfe514b6f4d0c852c6ecac93daba
MandyMF <mandy@fonoma.com>
2020-10-03 06:13:59 -0400

    Refactoring code and adding describes

--- tests/blogs_api.test.js
+++ tests/blogs_api.test.js
@@ -20,89 +20,92 @@ beforeEach(async () => {
   await Promise.all(blogPromises)
 })
 
-test('Returns correct amount of blogs in json format', async () => {
-  await api.get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+describe('get request tests', () => {
+  test('Returns correct amount of blogs in json format', async () => {
+    await api.get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
 
-  const blogsDb = await helper.blogsInDb()
-  expect(blogsDb).toHaveLength(helper.initialBlogs.length)
-})
-
-test('Blog post identifier property is named id and exist for every blog', async () => {
-  const blogsDb = await helper.blogsInDb()
-
-  blogsDb.forEach(blog => {
-    expect(blog.id).toBeDefined()
-  })
-})
-
-test('should add a blog after a post request',async () => {
-  const blog ={
-    title: 'title 5',
-    author: 'author 5',
-    url: 'url 5',
-    likes: 5,
-  }
-
-  await api.post('/api/blogs')
-    .send(blog)
-    .expect(201)
-  const blogList = await helper.blogsInDb()
-
-  expect(blogList).toHaveLength(helper.initialBlogs.length + 1)
-
-  const pure_blogs = blogList.map(i_blog => {
-    return { title:i_blog.title, author:i_blog.author, url:i_blog.url, likes:i_blog.likes }
+    const blogsDb = await helper.blogsInDb()
+    expect(blogsDb).toHaveLength(helper.initialBlogs.length)
   })
 
-  expect(pure_blogs).toContainEqual(blog)
+  test('Blog post identifier property is named id and exist for every blog', async () => {
+    const blogsDb = await helper.blogsInDb()
 
+    blogsDb.forEach(blog => {
+      expect(blog.id).toBeDefined()
+    })
+  })
 })
+describe('post request tests', () => {
+  test('should add a blog after a post request',async () => {
+    const blog ={
+      title: 'title 5',
+      author: 'author 5',
+      url: 'url 5',
+      likes: 5,
+    }
 
-test('on post if likes property is missing should default to 0', async () => {
-  const blog ={
-    title: 'title cerotest',
-    author: 'author cerotest',
-    url: 'url cerotest',
-  }
+    await api.post('/api/blogs')
+      .send(blog)
+      .expect(201)
+    const blogList = await helper.blogsInDb()
 
-  const resp_from_db = await api.post('/api/blogs')
-    .send(blog)
+    expect(blogList).toHaveLength(helper.initialBlogs.length + 1)
 
-  expect(resp_from_db.body.likes).toEqual(0)
+    const pure_blogs = blogList.map(i_blog => {
+      return { title:i_blog.title, author:i_blog.author, url:i_blog.url, likes:i_blog.likes }
+    })
 
-  const blogs = await helper.blogsInDb()
-  expect( blogs.find(blog => {
-    return blog.id === resp_from_db.body.id
-  }
-  ).likes
-  ).toEqual(0)
+    expect(pure_blogs).toContainEqual(blog)
+
+  })
+
+  test('on post if likes property is missing should default to 0', async () => {
+    const blog ={
+      title: 'title cerotest',
+      author: 'author cerotest',
+      url: 'url cerotest',
+    }
+
+    const resp_from_db = await api.post('/api/blogs')
+      .send(blog)
+
+    expect(resp_from_db.body.likes).toEqual(0)
+
+    const blogs = await helper.blogsInDb()
+    expect( blogs.find(blog => {
+      return blog.id === resp_from_db.body.id
+    }
+    ).likes
+    ).toEqual(0)
+  })
+
+  test('should return error 400 on if title and url are missing', async () => {
+    const blog = {
+      author: 'author cerotest',
+      likes: 3,
+    }
+    await api.post('/api/blogs')
+      .send(blog)
+      .expect(400)
+  })
 })
+describe('delete request tests', () => {
+  test('delete should work', async () => {
+    const initialBlogs = await helper.blogsInDb()
 
-test('should return error 400 on if title and url are missing', async () => {
-  const blog = {
-    author: 'author cerotest',
-    likes: 3,
-  }
-  await api.post('/api/blogs')
-    .send(blog)
-    .expect(400)
+    await api.delete(`/api/blogs/${initialBlogs[0].id}`)
+      .expect(204)
+
+    const afterDeleteBlogs = await helper.blogsInDb()
+
+    expect(afterDeleteBlogs).toHaveLength(initialBlogs.length - 1)
+    const res = await Blog.findById(initialBlogs[0].id)
+    expect(res).toEqual(null)
+  })
 })
-
-test('delete should work', async () => {
-  const initialBlogs = await helper.blogsInDb()
-
-  await api.delete(`/api/blogs/${initialBlogs[0].id}`)
-    .expect(204)
-
-  const afterDeleteBlogs = await helper.blogsInDb()
-
-  expect(afterDeleteBlogs).toHaveLength(initialBlogs.length - 1)
-  const res = await Blog.findById(initialBlogs[0].id)
-  expect(res).toEqual(null)
-})
-
 
 afterAll(() => {
   mongoose.connection.close()
